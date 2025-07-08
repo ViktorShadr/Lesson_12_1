2801450a1aa04f8ec0fca6c7ae08070598485434
Viktor Shadrin <v.vshadrin@gmail.com>
2025-07-08 22:54:50 +0300

    get_users.py needs to be completed

--- src/get_users.py
+++ src/get_users.py
@@ -1,3 +1,5 @@
+from xml.etree.ElementTree import indent
+
 import requests
 import os
 from dotenv import load_dotenv
@@ -8,10 +10,21 @@ def get_github_users(github_users):
     github_token = os.getenv('GITHUB_TOKEN')
     headers = {"Authorization": f"Bearer {github_token}"}
 
-    url = f"https://api.github.com/users/{github_users}"
-    response = requests.get(url, headers=headers)
-    user_info = response.json()
-    return user_info
+    url_user = f"https://api.github.com/users/{github_users}"
+    url_repos = f"https://api.github.com/users/{github_users}/repos"
+    response_user = requests.get(url_user, headers=headers)
+    response_repos = requests.get(url_repos, headers=headers)
+    user_info = response_user.json()
+    user_repos_info = response_repos.json()
+    keys_to_select = ['login', 'public_repos', 'name']
+    list_info = {}
+    for key in keys_to_select:
+        if key in user_info:
+            list_info[key] = user_info[key]
+
+    names = [user['name'] for user in user_repos_info.values()]
+
+    return list_info, names
 
 
 
